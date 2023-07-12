da59ce16b2b32dec2abac0a869bd5de7dcffa37c
Bassit <barseetbrown@gmail.com>
2023-07-12 16:43:16 +0100

    feat: added template files

---
+++ templates/express/default/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+//configure dotenv
+require("dotenv").config({ path: "./config/dev.env" });
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
+const app = express();
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+const publicDirectory = path.join(__dirname, "../public");
+app.use(express.static(publicDirectory));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+module.exports = app;

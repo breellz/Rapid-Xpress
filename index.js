a44da84c59d382ae635cd0ba0c2431a8b6e76815
Owolabi Bassit <52262467+breellz@users.noreply.github.com>
2025-02-11 15:54:33 +0100

    feat: bump version to 3.0.6

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const prog = require("caporal");
 const init = require("./lib/create");
 
 prog
- .version("3.0.5")
+ .version("3.0.6")
  .command("create", "Create a new application")
  .action(init);
 
